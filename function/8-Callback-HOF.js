19f3bb9bc61c78b0f26c4e5b21bf0d0dfb7a4144
Shruti Zambare <shruti.savaratkar@gmail.com>
2026-07-30 23:20:49 +0530

    Updated code

--- function/8-Callback-HOF.js
+++ function/8-Callback-HOF.js
@@ -79,7 +79,140 @@ console.log(calculateSalary(salary, addTax));
 //5. ========================== Greeting  user  ========================== . 
 let users = ["Rahul", "Anjali", "Ramesh"];
 
+//callback
+function helloConcat(user){
+    console.log("Hello " + user);
+}
+
 //HOF
 function greetUsers(arr, callback){
-    return arr;
+     return arr.forEach(callback);
 }
+
+greetUsers(users, helloConcat);
+
+
+//6. ========================== Build your own map() ========================== . 
+let numbers = [2,4,6,8,10];
+// callback Triple of every number
+function triple(number) {
+   return number * number * number;
+}
+
+// Half of every number
+function halfNumber(number) {
+   return number / 2;
+}
+
+// HOF
+function modifyArray(arr, callback) {
+   return numbers.map(callback);
+}
+console.log("Given Numbers" , numbers);
+console.log("triple Number:" , modifyArray(numbers, triple));
+console.log("half Number:" , modifyArray(numbers, halfNumber));
+
+// Q7. ========================== Discount Calculator ==========================
+
+let prices = [100,200,300,400];
+//callbak
+function discount10(price) {
+    return price - (price * 10 / 100);
+}
+
+function discount18(price) {
+    return price - (price * 18 / 100);
+}
+
+function gst(price) {
+    return  price + (price * 18 / 100);
+}
+
+
+
+// HOF
+function CalPrice(priceArray, callback) {
+    return priceArray.map(callback);
+}
+console.log("Price" , prices);
+console.log("10% Discount Prices:" , CalPrice(prices, discount10));
+console.log("18% Discount Prices:" , CalPrice(prices, discount18));
+console.log("Price after adding 18% gst" , CalPrice(prices, gst));
+
+
+// 8. ========================== Student Marks.==================================
+
+let marks = [35,48,70,90,22];
+
+//callback 
+function GraceMark(mark) {
+  return mark + 5;
+}
+
+//callback 
+function PercentageMark(mark){
+ return mark + "%";
+}
+
+//callback 
+function doubleMark(mark){
+   return mark  * 2 ;
+}
+//HOF
+function calMarks(marksarray, callback) {
+    return marksarray.map(callback);
+    
+}
+console.log("Marks:" , marks);
+console.log("Grace Marks:" , calMarks(marks, GraceMark));
+console.log("Percentage:", calMarks(marks, PercentageMark));
+console.log("Double Marks:", calMarks(marks, doubleMark));
+
+
+//9 . ==================================  String Modifier ================================== 
+let names = ["amit","rohit","pooja"];
+
+//callback
+function NameUppercase(nm) {
+  return nm.toUpperCase();
+}
+function NameLowercase(nm) {
+  return nm.toLowerCase();
+}
+
+// Add Mr./Ms.
+function NamePrifix(nm){
+    return ("Mr./Ms. " + nm);
+}
+
+//HOF
+function modifyNmes(nameArray, callback){
+   return nameArray.map(callback);
+}
+console.log("names:" , names);
+console.log("Names in Uppercase:" , modifyNmes(names, NameUppercase));
+console.log("Names in Lowercase:" , modifyNmes(names, NameLowercase));
+console.log("Names in Lowercase:" , modifyNmes(names, NamePrifix));
+
+
+//10. . ================================== Area Calculator . ==================================
+
+//callback
+function squareArea(length){
+   return length * length ;
+}
+function rectangleArea(width ,  height){
+   return width * height;
+}
+function circleArea(radious){
+  return  3.14 * radious * radious
+}
+
+//HOF
+function findArea(callback, ...values) {
+   return callback(...values);
+ 
+}
+console.log("Sqaure area : " , findArea(squareArea, 40));
+console.log("Rectangle  area : " , findArea(rectangleArea, 20, 30));
+console.log("circle area : " , findArea(circleArea, 17));
--- function/9-HOF.js
+++ function/9-HOF.js
@@ -67,5 +67,116 @@ let GraterNo = array7.filter((currentElement)=>{
 });
 console.log("No.s grater than 50" , GraterNo);
 
-// 8.
+//8 ..================================== Filter all negative numbers. ==================================
+
+let array8 = [14, -15, 0, 83, -10, 77, -46, 35];
+let NegativeNo = array8.filter((currentElement) => {
+    return currentElement < 0 ;
+});
+console.log("Negative" , NegativeNo);
+
+
+// 9. ==== ==== ==== ====  return only students who scored more than 60.======== ====
+let marks = [45, 89, 53, 87, 63, 50, 33];
+let marks60 = marks.filter((currentElement)=> {
+  return currentElement > 60;
+  });
+
+  console.log("students who scored more than 60" , marks60);
+
+// 10 =========================================== Return only products costing more than ₹1000.==================================
+
+let prices = [800, 1230, 850, 2500, 1500, 1200];
+
+let costlyPrice = prices.filter((currentPrice)=> {
+    return currentPrice > 1000; 
+});
+  console.log("products costing more than ₹1000 are :" , costlyPrice);
+
+  // ================================================= find() =========================================
+//find() returns the first element in an array that matches the given condition. If no element matches, it returns undefine 
+
+
+  // 11 ========= Find the first even number. .========
+
+let nums = [45, 89, 53, 87, 63, 50, 33];
+let FirstEven = nums.find((currentnum)=>{
+    return currentnum % 2 == 0;
+});
+
+console.log("the first even number:" , FirstEven);
+
+// 12 ======Find the first student whose marks are greater than 80.===
+let newMarks =[{name: "Student1" , marks: 80},
+               {name: "Student2", marks: 38},
+               {name: "Student3", marks: 68},
+               {name: "Student4", marks:46},
+               {name: "Student5", marks:99 },
+               {name: "Student6", marks:86},
+               {name: "Student6", marks:85},
+               {name: "Student7", marks:33}];
+let fistMarks = newMarks.find((currentMarks) => {
+    return currentMarks.marks > 80;
+});
+console.log("first student whose marks are greater than 80" , fistMarks);
+
+// Q13=========== Find the first negative number.==========
+
+let arrayNeg = [14, -15, 0, 83, -10, 77, -46, 35];
+let Firstnegative = arrayNeg.find((currentnum)=>{
+    return currentnum <= 0;
+});
+console.log("the first negative number:" , Firstnegative);
+
+
+// ================================================= Some() =========================================
+//some() returns the 
+
+//14 === Check whether the array contains any negative number.=======
+let anyNeg = arrayNeg.some((currentno)=>{
+    return currentno < 0;
+});
+console.log("array contains any negative number" , anyNeg);
+
+//Q15 === Check whether any student scored 100.  ======
+let student = newMarks.some((currentMarks)=>{
+    return currentMarks.marks = 0;
+});
+console.log("student scored 100" , student);
+
+//Q16 ===== Check whether any product costs more than ₹5000 ======
+let pricesCost = prices.some((currentPrice)=>{
+    return currentPrice > 5000;
+});
+console.log("any product costs more than ₹5000 ", pricesCost);
+
+
+
+// ================================================= every() =========================================
+
+//Q17 Check whether all numbers are positive.
+let noArray = [55, 58, -8, 23, 69, -98];
+let postiveNoArray = noArray.every((currentElement)=>{
+    return currentElement > 0;
+});
+console.log("all numbers are positive " , postiveNoArray);
+
+
+// Q18 Check whether all students passed (pass marks 35)
+let Subjectmarks = [45, 89, 53, 87, 63, 50, 39];
+let StudentPass = Subjectmarks.every((currentElement)=>{
+    return currentElement > 35;
+});
+console.log("all students passed " , StudentPass);
+
+// ================================================= sort() =========================================
+//19. Sort the following numbers in ascending and descending order.
+
+let givenArray =[45,12,78,1,99,25];
+
+let ascending = [...givenArray].sort((a, b) => a - b);
+let descending = [...givenArray].sort((a, b) => b - a);
+
+console.log("Ascending:", ascending);
+console.log("Descending:", descending);
 
